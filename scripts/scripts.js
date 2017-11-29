a93b22ddd567d1cde385ece0c0fe86daced431bb
GRIANT <agrijalva@bism.com.mx>
2017-11-29 17:34:11 -0600

    Reset script.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,55 +12,6 @@ var app = angular.module("yapp", ["ui.router", "ngAnimate", "ngSanitize","ui.car
         templateUrl: "pages/login/login.html",
         controller: "LoginCtrl"
     })
-    .state("admin", {
-        url: "/admin",
-        parent: "base",
-        cache:false,
-        templateUrl: "pages/admin.html",
-        controller: "DashboardCtrl"
-    })
-    .state("agentes", {
-        url: "/agentes",
-        parent: "admin",
-        cache:false,
-        templateUrl: "pages/agentes/templates/agentes.html",
-        controller: "AgentesCtrl"
-    })
-    .state("agente_detalle", {
-        url: "/agente_detalle",
-        parent: "admin",
-        cache:false,
-        templateUrl: "pages/agentes/templates/editar_agente.html",
-        controller: "AgentesCtrl"
-    })
-    .state("nuevo_agente", {
-        url: "/nuevo_agente",
-        parent: "admin",
-        cache:false,
-        templateUrl: "pages/agentes/templates/nuevo_agente.html",
-        controller: "AgentesCtrl"
-    })
-    .state("clientes", {
-        url: "/clientes",
-        parent: "admin",
-        cache:false,
-        templateUrl: "pages/clientes/templates/clientes.html",
-        controller: "ClientesCtrl"
-    })
-    .state("nuevo_cliente", {
-        url: "/nuevo_cliente",
-        parent: "admin",
-        cache:false,
-        templateUrl: "pages/clientes/templates/nuevo_cliente.html",
-        controller: "ClientesCtrl"
-    })
-    .state("cliente_detalle", {
-        url: "/cliente_detalle",
-        parent: "admin",
-        cache:false,
-        templateUrl: "pages/clientes/templates/cliente_detalle.html",
-        controller: "ClientesCtrl"
-    })
 }]);
 
 var API_Path = "http://localhost/APIForrajera/v1/index.php"
